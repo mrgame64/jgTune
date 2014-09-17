d611a7d7bdf0686b36a48c9b7c6955278c7ee4ba
James Game <james.game.64@gmail.com>
2014-09-17 22:11:29 +0200

    Added TODOs

--- src/org/jgdev/tune/MainActivity.java
+++ src/org/jgdev/tune/MainActivity.java
@@ -4,7 +4,9 @@ public abstract class MainActivity
 {
 
 	public static void main(String[] args) {
-		
+		//TODO: define a file format for chiptunes
+		//TODO: write a chiptune file interpreter
+		//TODO: consider a visualizer in a separate repo?
 	}
 
 }

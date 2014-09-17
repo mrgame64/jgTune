5a6dc2406c36aa7ba3b766350901f3118b0e32ac
James Game <james.game.64@gmail.com>
2014-09-17 21:26:24 +0200

    Implemented the square wave synth

---
+++ src/org/jgdev/tune/waves/Square.java
@@ -0,0 +1,29 @@
+package org.jgdev.tune.waves;
+
+public class Square implements Wave {
+
+	@Override
+	public byte[] getSamples(int n) {
+		byte[] samples = new byte[n];
+		
+		for(int t=0; t<n; t++) {
+			float x = t/n;
+			
+			float y = (float) (1 - Math.floor(x*2) % 2);
+			samples[t] = (byte) (Math.round(y*127) - 255);
+		}
+		
+		return samples;
+	}
+
+	@Override
+	public byte getSample(float freq, long millis) {
+		if(millis > 1000) millis -= 1000;
+		
+		float x = millis/1000f;
+		
+		float y = (float) (1 - Math.floor(x*2*freq) % 2);
+		return (byte) (Math.round(y*127) - 255);
+	}
+
+}

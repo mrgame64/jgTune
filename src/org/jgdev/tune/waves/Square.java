19df6e1a469635146582e412c23e0992163f45e8
James Game <james.game.64@gmail.com>
2014-09-17 22:00:21 +0200

    Changed implementation to reduce code redundancy

--- src/org/jgdev/tune/waves/Square.java
+++ src/org/jgdev/tune/waves/Square.java
@@ -6,21 +6,20 @@ public class Square implements Wave {
 	public byte[] getSamples(int n) {
 		byte[] samples = new byte[n];
 		
-		for(int t=0; t<n; t++) {
-			float x = (float)(t)/n;
-			
-			float y = (float) (1 - Math.floor(x*2) % 2);
-			samples[t] = (byte) (Math.round(y*255) - 128);
-		}
+		for(int t=0; t<n; t++)
+			samples[t] = getSample(1f, (float)(t)/n);
 		
 		return samples;
 	}
 
 	@Override
 	public byte getSample(float freq, long millis) {
-		if(millis > 1000) millis -= 1000;
-		
-		float x = millis/1000f;
+		return getSample(freq, millis/1000f);
+	}
+
+	@Override
+	public byte getSample(float freq, float sec) {
+		float x = sec % (1f + Float.MIN_NORMAL);
 		float y = (float) (1 - Math.floor(x*2*freq) % 2);
 		return (byte) (Math.round(y*255) - 128);
 	}
--- src/org/jgdev/tune/waves/Wave.java
+++ src/org/jgdev/tune/waves/Wave.java
@@ -3,4 +3,5 @@ package org.jgdev.tune.waves;
 public interface Wave {
 	public byte[] getSamples(int n);
 	public byte getSample(float freq, long millis);
+	public byte getSample(float freq, float sec);
 }
